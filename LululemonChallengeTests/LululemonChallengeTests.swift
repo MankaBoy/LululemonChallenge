b9c8bc13c5150a2dd704e4a0ccfb487ca811dd68
Noor Ali <profesanet@gmail.com>
2023-05-16 15:02:30 -0500

    Unit Test cases for core-Data

--- LululemonChallengeTests/LululemonChallengeTests.swift
+++ LululemonChallengeTests/LululemonChallengeTests.swift
@@ -9,39 +9,21 @@ import XCTest
 import CoreData
 @testable import LululemonChallenge
 class LululemonChallengeTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    
+    let itemsViewModel =  ItemsViewModel()
+    let addItemViewModel = AddItemViewModel()
+    let editItemViewModel = EditItemViewModel()
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        restoreDB()
+        restoreDataBase()
         
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
     func testFetchAndSaveData() {
-        let itemsViewModel = ItemsViewModel()
-        let addItemViewModel = AddItemViewModel()
-        var originalCount = 0
-        itemsViewModel.fetchDataFromDatabase(sortIndex: 1)
-        originalCount = itemsViewModel.items.count
-        
         addItemViewModel.saveDataToCoreData(name: "testGarment1")
-        itemsViewModel.fetchDataFromDatabase(sortIndex: 1)
-        XCTAssertEqual(itemsViewModel.getItemAtIndex(index: originalCount)?.name, "testGarment1")
+        itemsViewModel.fetchDataFromDatabase(sortIndex: 0)
+        XCTAssertEqual(itemsViewModel.items.last?.name, "testGarment1")
     }
     
     func testViewModelGetCount() {
@@ -51,38 +33,37 @@ class LululemonChallengeTests: XCTestCase {
     }
     
     func testViewModelDeleteData() {
-        let itemsViewModel =  ItemsViewModel()
-        let addItemViewModel = AddItemViewModel()
+        itemsViewModel.fetchDataFromDatabase(sortIndex: 1)
+        var  originalCount = itemsViewModel.getItemsCount()
         addItemViewModel.saveDataToCoreData(name: "testGarment1")
         addItemViewModel.saveDataToCoreData(name: "testGarment2")
         addItemViewModel.saveDataToCoreData(name: "testGarment3")
 
         itemsViewModel.fetchDataFromDatabase(sortIndex: 1)
-        XCTAssertEqual(itemsViewModel.getItemsCount(), 3)
+        originalCount += 3
+        XCTAssertEqual(itemsViewModel.getItemsCount(), originalCount)
         let item = itemsViewModel.getItemAtIndex(index: 0)
         if let item = item {
             itemsViewModel.deleteItemAtIndex(item: item)
         }
         itemsViewModel.fetchDataFromDatabase(sortIndex: 1)
-        XCTAssertEqual(itemsViewModel.getItemsCount(), 2)
+        XCTAssertEqual(itemsViewModel.getItemsCount(), originalCount - 1)
     }
     
     func testEditItem() {
-        let itemsViewModel = ItemsViewModel()
-        let addItemViewModel = AddItemViewModel()
         addItemViewModel.saveDataToCoreData(name: "testGarment12")
         addItemViewModel.saveDataToCoreData(name: "testGarmnet2")
         itemsViewModel.fetchDataFromDatabase(sortIndex: 1)
-        let editItemViewModel = EditItemViewModel()
         let itemToEdit = itemsViewModel.getItemAtIndex(index: 0)
         if let itemToEdit = itemToEdit {
-            editItemViewModel.editItem(item: itemToEdit, newName: "TestGarmentNew")
+            editItemViewModel.editItem(item: itemToEdit, newName: "testGarmentNew")
         }
         itemsViewModel.fetchDataFromDatabase(sortIndex: 1)
-        XCTAssertEqual(itemsViewModel.getItemAtIndex(index: 0)?.name, "TestGarmentNew")
+        XCTAssertEqual(itemsViewModel.getItemAtIndex(index: 0)?.name, "testGarmentNew")
     }
     
-    func restoreDB() {
+    
+    func restoreDataBase() {
         let dbManager = DataBaseManager.shared
         let request = Item.fetchRequest() as NSFetchRequest<Item>
         request.predicate = NSPredicate(format: "name BEGINSWITH[c] %@", "test")
